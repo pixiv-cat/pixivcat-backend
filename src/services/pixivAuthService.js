4fbc0108c03fbd1303959883e99fe5070ca4232f
jongcs <21971007+jongcs@users.noreply.github.com>
2024-03-14 23:49:02 +0800

    Lock access token index during refresh
    Rename rotateAccessTokenIndex() to getAccessTokenIndex()

--- src/services/pixivAuthService.js
+++ src/services/pixivAuthService.js
@@ -42,35 +42,37 @@ const refreshToken = async (refresh_token) => {
   return response.data.response;
 }
 
-const rotateAccessToken = () => {
+const getAccessTokenIndex = () => {
+  // Rotate index
   currentTokenIndex = (currentTokenIndex + 1) % pixivAuth.length;
+  return currentTokenIndex;
 };
 
 const getAccessToken = async () => {
-  rotateAccessToken();
+  const tokenIndex = getAccessTokenIndex();
 
-  if (pixivAuth[currentTokenIndex].expireTimestamp < Date.now()) {
-    if (!pixivAuth[currentTokenIndex].refreshing) {
+  if (pixivAuth[tokenIndex].expireTimestamp < Date.now()) {
+    if (!pixivAuth[tokenIndex].refreshing) {
       // Set the refreshing flag to indicate that a refresh is in progress
-      pixivAuth[currentTokenIndex].refreshing = true;
+      pixivAuth[tokenIndex].refreshing = true;
 
       try {
-        const refreshRes = await refreshToken(pixivAuth[currentTokenIndex].refreshToken);
-        pixivAuth[currentTokenIndex].accessToken = refreshRes.access_token;
-        pixivAuth[currentTokenIndex].refreshToken = refreshRes.refresh_token;
-        pixivAuth[currentTokenIndex].expireTimestamp = Date.now() + (refreshRes.expires_in * 0.9) * 1000;
-        console.log(`Pixiv access token[${currentTokenIndex}] refreshed`);
+        const refreshRes = await refreshToken(pixivAuth[tokenIndex].refreshToken);
+        pixivAuth[tokenIndex].accessToken = refreshRes.access_token;
+        pixivAuth[tokenIndex].refreshToken = refreshRes.refresh_token;
+        pixivAuth[tokenIndex].expireTimestamp = Date.now() + (refreshRes.expires_in * 0.9) * 1000;
+        console.log(`Pixiv access token[${tokenIndex}] refreshed`);
       } catch (err) {
         console.log('Pixiv refresh token failed.', err);
       } finally {
         // Reset the refreshing flag when the refresh is completed (whether successful or not)
-        pixivAuth[currentTokenIndex].refreshing = false;
+        pixivAuth[tokenIndex].refreshing = false;
       }
     } else {
       // If another refresh is already in progress, wait for its completion
       await new Promise(resolve => {
         const interval = setInterval(() => {
-          if (!pixivAuth[currentTokenIndex].refreshing) {
+          if (!pixivAuth[tokenIndex].refreshing) {
             clearInterval(interval);
             resolve();
           }
@@ -79,7 +81,7 @@ const getAccessToken = async () => {
     }
   }
 
-  return pixivAuth[currentTokenIndex].accessToken;
+  return pixivAuth[tokenIndex].accessToken;
 };
 
 module.exports = {
